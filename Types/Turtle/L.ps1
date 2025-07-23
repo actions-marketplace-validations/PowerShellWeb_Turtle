caa1909628ce707ff3d1fbef988ca22261714ca4
James Brundage <+@noreply.github.com>
2025-07-23 16:41:34 -0700

    feat: `Turtle.L()` ( Fixes #20 )

---
+++ Types/Turtle/L.ps1
@@ -0,0 +1,47 @@
+param(
+[Alias('Start', 'StartString', 'Initiator')]
+[string]
+$Axiom,
+
+[Alias('Rules', 'ProductionRules')]
+[Collections.IDictionary]
+$Rule = [Ordered]@{},
+
+[Alias('Iterations', 'Steps', 'IterationCount','StepCount')]
+[int]
+$N = 2,
+
+[Collections.IDictionary]
+$Variable = @{}
+)
+
+if ($n -le 1) { return $Axiom}
+
+$currentState = "$Axiom"
+$combinedPattern = "(?>$($Rule.Keys -join '|'))"    
+foreach ($iteration in 1..$n) {
+    $currentState = $currentState -replace $combinedPattern, {
+        $match = $_
+        $matchingRule = $rule["$match"]
+        if ($matchingRule -is [ScriptBlock]) {
+            return "$(& $matchingRule $match)"
+        } else {
+            return $matchingRule
+        }
+    }    
+}
+
+$finalState = $currentState
+foreach ($character in $finalState.ToCharArray()) {
+    foreach ($key in $Variable.Keys) {
+        if ($character -match $key) {
+            $action = $Variable[$key]
+            if ($action -is [ScriptBlock]) {
+                . $action $character
+            } else {
+                $action
+            } 
+        }
+    }
+}
+return

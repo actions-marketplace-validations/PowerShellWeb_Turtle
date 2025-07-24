3d6c4307c8a640422816a1563f4297fd7dcca721
James Brundage <+@noreply.github.com>
2025-07-24 14:13:55 -0700

    fix: Explicitly clearing position ( Fixes #30 )
    This impacts ViewBox caculations on repeated executions, since the effective start position of the cursor is maintained.

--- Types/Turtle/Clear.ps1
+++ Types/Turtle/Clear.ps1
@@ -1,6 +1,6 @@
-$this.Position = 0, 0
 $this.Heading = 0
 $this.Steps = @()
+$this | Add-Member -MemberType NoteProperty -Force -Name '.Position' -Value ([pscustomobject]@{ X = 0; Y = 0 })
 $this | Add-Member -MemberType NoteProperty -Force -Name '.Minimum' -Value ([pscustomobject]@{ X = 0; Y = 0 })
 $this | Add-Member -MemberType NoteProperty -Force -Name '.Maximum' -Value ([pscustomobject]@{ X = 0; Y = 0 })
 $this.ViewBox = 0

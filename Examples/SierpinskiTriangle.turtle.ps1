e7988b3ccec686f3c6c10dd220fc62d2b653d0b6
James Brundage <+@noreply.github.com>
2025-07-28 11:24:55 -0700

    test: Testing GitHub Action ( re #66 )
    Fixing parameter names

--- Examples/SierpinskiTriangle.turtle.ps1
+++ Examples/SierpinskiTriangle.turtle.ps1
@@ -2,7 +2,5 @@ $myName = $MyInvocation.MyCommand.Name -replace '\.turtle\.ps1$'
 $turtle = 
     Move-Turtle SierpinskiTriangle 15 5 |
     Set-Turtle -Property Stroke -Value '#4488ff'
-
-$turtle | Save-Turtle -Name "./$myName.svg"
-
-$turtle | Save-Turtle -Name "./$myName.png" -Property PNG
+$turtle | Save-Turtle "./$myName.svg" 
+$turtle | Save-Turtle "./$myName.png" -Property PNG

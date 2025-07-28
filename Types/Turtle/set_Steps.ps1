cec445ff5b8d927fd32101361eca4645ec197180
James Brundage <+@noreply.github.com>
2025-07-28 14:22:49 -0700

    docs: Documenting Turtle.set_Steps ( Fixes #8 )

--- Types/Turtle/set_Steps.ps1
+++ Types/Turtle/set_Steps.ps1
@@ -1,6 +1,14 @@
+<#
+.SYNOPSIS
+    Sets the steps of the turtle.
+.DESCRIPTION
+    Sets the steps of the turtle to the specified array of strings.
+
+    This property will rarely be set directly, but will be updated every time the turtle moves.    
+#>
 param(
 [string[]]
 $Steps
 )
 
-$this |  Add-Member -MemberType NoteProperty -Force -Name '.Steps' -Value $Steps
+$this |  Add-Member -MemberType NoteProperty -Force -Name '.Steps' -Value @($Steps)

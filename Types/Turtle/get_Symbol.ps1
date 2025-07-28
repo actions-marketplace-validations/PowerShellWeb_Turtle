7a2165a7c29209a8cbc45341419299700fbafebb
James Brundage <+@noreply.github.com>
2025-07-28 11:43:06 -0700

    docs: `Turtle.get_Symbol` ( Fixes #19 )
    Adding docs and transforming around the center

--- Types/Turtle/get_Symbol.ps1
+++ Types/Turtle/get_Symbol.ps1
@@ -1,10 +1,23 @@
+<#
+.SYNOPSIS
+    Gets the Turtle as a symbol.
+.DESCRIPTION
+    Returns the turtle as an SVG symbol element, which can be used in other SVG files.
+
+    Symbols allow a shape to be scaled and reused without having the duplicate the drawing commands.
+
+    By default, this will return a SVG defining the symbol and using it to fill the viewport.
+.EXAMPLE
+    Move-Turtle Flower |
+        Select-Object -ExpandProperty Symbol
+#>
 param()
 
 @(    
-    "<svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>"
+    "<svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%' transform-origin='50% 50%'>"
         "<symbol id='turtle-symbol' viewBox='$($this.ViewBox)' transform-origin='50% 50%'>"
             $this.PathElement.OuterXml
         "</symbol>"
-        "<use href='#turtle-symbol' width='100%' height='100%' />"
+        "<use href='#turtle-symbol' width='100%' height='100%' transform-origin='50% 50%' />"
     "</svg>"
 ) -join '' -as [xml]

b28677103d0ba639a7ebf2536b1c7f7e60c02259
James Brundage <+@noreply.github.com>
2025-07-28 12:17:13 -0700

    docs: `Turtle.LSystem` docs ( Fixes #20 )
    Also forcing scriptblocks to be locally recreated

--- Types/Turtle/LSystem.ps1
+++ Types/Turtle/LSystem.ps1
@@ -1,3 +1,108 @@
+<#
+.SYNOPSIS
+    Draws a L-system pattern.
+.DESCRIPTION
+    Generates a pattern using a L-system.
+
+    The initial string (Axiom) is transformed according to the rules provided for a specified number of iterations.    
+.LINK
+    https://en.wikipedia.org/wiki/L-system
+.EXAMPLE        
+    # Box Fractal L-System
+    $Box = 'F-F-F-F'
+    $Fractal = 'F-F+F+F-F'
+    
+    $turtle.Clear().LSystem(
+            $Box, 
+            [Ordered]@{ F = $Fractal },
+            3, 
+            @{
+                F = { $this.Forward(10) }
+                J = { $this.Jump(10) }
+                '\+' = { $this.Rotate(90) }            
+                '-' = { $this.Rotate(-90) }
+            }
+    ).Pattern.Save("$pwd/BoxFractalLSystem.svg")
+.EXAMPLE
+    # Fractal L-System
+    $Box = 'FFFF-FFFF-FFFF-FFFF' 
+    $Fractal = 'F-F+F+F-F'
+        
+    $turtle.Clear().LSystem(
+            $Box, 
+            [Ordered]@{ F = $Fractal },
+            4, 
+            @{
+                F = { $this.Forward(10) }
+                J = { $this.Jump(10) }
+                '\+' = { $this.Rotate(90) }            
+                '-' = { $this.Rotate(-90) }
+            }
+    ).Symbol.Save("$pwd/FractalLSystem.svg")
+.EXAMPLE
+    # Arrowhead Fractal L-System
+    $Box = 'FF-FF-FF' 
+    $Fractal = 'F-F+F+F-F'
+    
+    
+    $turtle.Clear().LSystem(
+            $Box, 
+            [Ordered]@{ F = $Fractal },
+            4, 
+            @{
+                F = { $this.Forward(10) }
+                J = { $this.Jump(10) }
+                '\+' = { $this.Rotate(90) }            
+                '-' = { $this.Rotate(-90) }
+            }
+    ).Pattern.Save("$pwd/ArrowheadFractalLSystem.svg")
+.EXAMPLE
+    # Tetroid LSystem
+    $turtle.Clear().LSystem(
+            'F', 
+            [Ordered]@{ F = 'F+F+F+F' + 
+                '+JJJJ+' + 
+                'F+F+F+F' + 
+                '++JJJJ' +
+                'F+F+F+F' +
+                '++JJJJ' +
+                'F+F+F+F' +
+                '++JJJJ' + 
+                '-JJJJ'
+            },                
+            3, 
+            @{
+                F = { $this.Forward(10) }
+                J = { $this.Jump(10) }
+                '\+' = { $this.Rotate(90) }            
+                '-' = { $this.Rotate(-90) }
+            }
+    ).Pattern.Save("$pwd/TetroidLSystem.svg")
+
+.EXAMPLE
+    $turtle.Clear().LSystem(
+        'F', 
+        [Ordered]@{ F = '
+F+F+F+F +JJJJ+ F+F+F+F ++ JJJJ' },
+        3, 
+        @{
+            F = { $this.Forward(10) }
+            J = { $this.Jump(10) }
+            '\+' = { $this.Rotate(90) }            
+            '-' = { $this.Rotate(-90) }
+        }
+    ).Pattern.Save("$pwd/LSystemCool1.svg")
+.EXAMPLE
+    Move-Turtle LSystem F-F-F-F ([Ordered]@{F='F-F+F+F-F'}) 3 (
+        [Ordered]@{
+            F = { $this.Forward(10) }
+            J = { $this.Jump(10) }
+            '\+' = { $this.Rotate(90) }            
+            '-' = { $this.Rotate(-90) }
+        }
+    )
+    
+#>
 param(
 [Alias('Start', 'StartString', 'Initiator')]
 [string]
@@ -15,7 +120,7 @@ $N = 2,
 $Variable = @{}
 )
 
-if ($n -le 1) { return $Axiom}
+if ($n -lt 1) { return $Axiom}
 
 $currentState = "$Axiom"
 $combinedPattern = "(?>$($Rule.Keys -join '|'))"    
@@ -23,7 +128,7 @@ foreach ($iteration in 1..$n) {
     $currentState = $currentState -replace $combinedPattern, {
         $match = $_
         $matchingRule = $rule["$match"]
-        if ($matchingRule -is [ScriptBlock]) {
+        if ($matchingRule -is [ScriptBlock]) {                        
             return "$(& $matchingRule $match)"
         } else {
             return $matchingRule
@@ -31,11 +136,21 @@ foreach ($iteration in 1..$n) {
     }    
 }
 
+$localReplacement = [Ordered]@{}
+foreach ($key in $variable.Keys) {
+    $localReplacement[$key] = 
+        if ($variable[$key] -is [ScriptBlock]) {
+            [ScriptBlock]::Create($variable[$key])
+        } else {
+            $variable[$key]
+        }
+}
+
 $finalState = $currentState
 $null = foreach ($character in $finalState.ToCharArray()) {
     foreach ($key in $Variable.Keys) {
         if ($character -match $key) {
-            $action = $Variable[$key]
+            $action = $localReplacement[$key]
             if ($action -is [ScriptBlock]) {
                 . $action $character
             } else {

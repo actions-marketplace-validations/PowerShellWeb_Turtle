ce32521db93e22522559437d9fa16b3866063c3e
James Brundage <james.brundage@start-automating.com>
2025-07-29 11:36:28 -0700

    docs: Updating EndlessSnowflake to use dictionary animations

--- Examples/EndlessSnowflake.turtle.ps1
+++ Examples/EndlessSnowflake.turtle.ps1
@@ -2,22 +2,17 @@ Push-Location $PSScriptRoot
 $turtle = turtle KochSnowflake 10 4 | 
     Set-Turtle -Property PatternTransform -Value @{scale=0.33} |
     set-turtle -property Fill -value '#4488ff' |
-    Set-Turtle -Property PatternAnimation -Value "
-
-    <animateTransform attributeName='patternTransform' attributeType='XML' type='scale' values='0.66;0.33;0.66' dur='19s' repeatCount='indefinite' additive='sum' />
-
-    <animateTransform attributeName='patternTransform' attributeType='XML' type='rotate' from='0' to='360' dur='29s' repeatCount='indefinite' additive='sum' />
-
-    <animateTransform attributeName='patternTransform' attributeType='XML' type='skewX' values='30;-30;30' dur='67s' repeatCount='indefinite' additive='sum' />
-
-    <animateTransform attributeName='patternTransform' attributeType='XML' type='skewY' values='30;-30;30' dur='83s' repeatCount='indefinite' additive='sum' />
-
-    <animateTransform attributeName='patternTransform' attributeType='XML' type='translate' values='0 0;42 42;0 0' dur='103s' repeatCount='indefinite' additive='sum' />
-
-    "
-    
-
-
+    Set-Turtle -Property PatternAnimation -Value ([Ordered]@{
+        type = 'scale'    ; values = 0.66,0.33, 0.66 ; repeatCount = 'indefinite' ;dur = "23s"; additive = 'sum'
+    }, [Ordered]@{
+        type = 'rotate'   ; values = 0, 360 ;repeatCount = 'indefinite'; dur = "41s"; additive = 'sum'
+    }, [Ordered]@{
+        type = 'skewX'    ; values = -30,30,-30;repeatCount = 'indefinite';dur = "83s";additive = 'sum'
+    }, [Ordered]@{
+        type = 'skewY'    ; values = 30,-30, 30;repeatCount = 'indefinite';additive = 'sum';dur = "103s"
+    }, [Ordered]@{
+        type = 'translate';values = "0 0","42 42", "0 0";repeatCount = 'indefinite';additive = 'sum';dur = "117s"
+    })    
     
 $turtle | save-turtle -Path ./EndlessSnowflake.svg -Property Pattern
 Pop-Location

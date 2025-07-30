03f6c630b2b2dd857bbc5e9689c17681e9060697
Jake Bolton <ninmonkeys@gmail.com>
2025-07-30 11:41:17 -0500

    fix: `Turtle.get_Position` missing parenthesis on value ( Fixes #85 )

--- Types/Turtle/get_Position.ps1
+++ Types/Turtle/get_Position.ps1
@@ -1,4 +1,4 @@
 if (-not $this.'.Position') {
-    $this |  Add-Member -MemberType NoteProperty -Force -Name '.Position' -Value [pscustomobject]@{ X = 0; Y = 0 }
+    $this |  Add-Member -MemberType NoteProperty -Force -Name '.Position' -Value ([pscustomobject]@{ X = 0; Y = 0 })
 }
 return $this.'.Position'

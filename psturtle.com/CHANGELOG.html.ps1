b96d54387d03d310f35803c7890b47f547f337f4
StartAutomating <james.brundage@start-automating.com>
2025-08-28 22:50:36 -0700

    feat: https://psturtle.com/CHANGELOG ( Fixes #176 )
    Linking to issues

--- psturtle.com/CHANGELOG.html.ps1
+++ psturtle.com/CHANGELOG.html.ps1
@@ -1,5 +1,18 @@
+param(
+[uri]
+$RepositoryUrl = "https://github.com/PowerShellWeb/Turtle"
+)
 if ($PSScriptRoot) { Push-Location $PSScriptRoot}
-Get-ChildItem -Path ../CHANGELOG.md | 
+(Get-ChildItem -Path ../CHANGELOG.md | 
     ConvertFrom-Markdown |
-    Select-Object -ExpandProperty HTML
+    Select-Object -ExpandProperty HTML) -replace '(?<=[\(\,)]\s{0,})\#\d+', {
+        $match = $_                
+        if ($RepositoryUrl) {            
+            "<a href='$RepositoryUrl/issues/$($match -replace '\#')>"
+            "$match"
+            "</a>"
+        } else {
+            "$match"
+        }        
+    }
 if ($PSScriptRoot) { Pop-Location}

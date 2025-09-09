04785104cabaf8ef6c31d2adf6dde9846f6b9e15
StartAutomating <james.brundage@start-automating.com>
2025-09-08 18:13:12 -0700

    docs: Adding Turtles All The Way Down to README ( Fixes #206 )
    Fixing inscribed circle example (forcing ordering)

--- Examples/InscribedCircle.turtle.ps1
+++ Examples/InscribedCircle.turtle.ps1
@@ -5,10 +5,10 @@
     A simple example of turtles containing turtles
 #>
 $inscribedCircle = 
-    turtle width 42 height 42 turtles @{
+    turtle width 42 height 42 turtles ([Ordered]@{
         'square' = turtle square 42 fill '#4488ff' stroke '#224488'
         'circle' = turtle circle 21 fill '#224488' stroke '#4488ff'
-    } 
+    })
 
 $inscribedCircle | Save-Turtle ./InscribedCircle.svg
 $inscribedCircle | Save-Turtle ./InscribedCirclePattern.svg Pattern

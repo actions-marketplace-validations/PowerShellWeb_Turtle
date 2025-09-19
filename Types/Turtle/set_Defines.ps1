7479c17f4919b8f3576b1c7ece53f09330caef99
StartAutomating <james.brundage@start-automating.com>
2025-09-19 14:20:50 -0700

    feat: `Turtle.Defines` ( Fixes #243 )
    Adding examples and fixing functionality.

--- Types/Turtle/set_Defines.ps1
+++ Types/Turtle/set_Defines.ps1
@@ -3,6 +3,41 @@
     Sets the Turtle Path Animation
 .DESCRIPTION
     Sets an animation for the Turtle path.
+.EXAMPLE
+    $t = turtle defines @(
+        "<radialGradient id='gradient'>
+            <stop offset='33%' stop-color='red' />
+            <stop offset='66%' stop-color='green' />
+            <stop offset='100%' stop-color='blue' />
+        </radialGradient>"
+        "<radialGradient id='gradient2'>
+            <stop offset='33%' stop-color='blue' />
+            <stop offset='66%' stop-color='green' />
+            <stop offset='100%' stop-color='red' />
+        </radialGradient>"
+
+    ) flower 42 fill 'url("#gradient")' stroke 'url("#gradient2")'     
+    $t | turtle save ./gradient.svg
+.EXAMPLE
+    $t = turtle defines @(
+        "<radialGradient id='gradient'>
+            <stop offset='33%' stop-color='red' />
+            <stop offset='66%' stop-color='green' />
+            <stop offset='100%' stop-color='blue' />
+        </radialGradient>"
+        "<radialGradient id='gradient2'>
+            <stop offset='33%' stop-color='blue' />
+            <stop offset='66%' stop-color='green' />
+            <stop offset='100%' stop-color='red' />
+        </radialGradient>"        
+    ) width 100 height 100 teleport 50 50 StarFlower 42 14.4 6 25 fill 'url("#gradient")' stroke 'url("#gradient2")' fillrule evenodd morph @(
+        turtle teleport 50 50 StarFlower 42 12 5 30
+        turtle teleport 50 50 StarFlower 42 14.4 6 25
+        turtle teleport 50 50 StarFlower 42 12 5 30
+    ) PathAnimation ( [Ordered]@{
+        type = 'rotate'   ; values = 0, 360 ;repeatCount = 'indefinite'; dur = "4.2s"
+    }) 
+    $t | turtle save ./gradientrotate.svg
 #>
 param(
 # The definition object.
@@ -12,7 +47,7 @@ $Defines
 )
 
 $newDefinition = @(foreach ($definition in $Defines) {
-    if ($ -is [Collections.IDictionary]) {
+    if ($definition -is [Collections.IDictionary]) {
         $definitionCopy = [Ordered]@{} + $definition                
         "<$elementName $(
             @(foreach ($key in $definitionCopy.Keys) {

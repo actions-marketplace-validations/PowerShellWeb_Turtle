d8627a76ba893d91ac6a585f54c729ee9945b4df
StartAutomating <james.brundage@start-automating.com>
2025-09-29 15:25:09 -0700

    feat: `Turtle.get_TextElement` ( Fixes #265 )
    Autocentering Turtle Text without a path

--- Types/Turtle/get_TextElement.ps1
+++ Types/Turtle/get_TextElement.ps1
@@ -1,14 +1,55 @@
+<#
+.SYNOPSIS
+    Gets a Turtle's text element
+.DESCRIPTION
+    Gets a Turtle's text as a SVG Text element.
 
-if ($this.Text) {
-    return @(
-        "<text id='$($this.ID)-text' $(
-    foreach ($TextAttributeName in $this.TextAttribute.Keys) {
-        " $TextAttributeName='$($this.TextAttribute[$TextAttributeName])'"
+    If the Turtle does not have any text, this will return nothing.
+
+    If the Turtle has text, but no path, the text will be centered in the Turtle's viewbox.
+.LINK
+    https://developer.mozilla.org/en-US/docs/Web/SVG/Reference/Element/text
+.EXAMPLE
+    turtle text "hello world" textElement
+#>
+
+
+if (-not $this.Text) { return }
+
+$textAttributes = [Ordered]@{}
+
+if (-not $this.Steps) {
+    $textAttributes['dominant-baseline'] = 'middle'
+    $textAttributes['text-anchor'] = 'middle'
+    $textAttributes['x'] = '50%'
+    $textAttributes['y'] = '50%'
+}
+
+foreach ($collection in 'SVGAttribute','Attribute') {
+    foreach ($key in $this.$collection.Keys) {
+        if ($key -match '^text/') {
+            $textAttributes[$key -replace '^text/'] = $this.$collection[$key]
+        }
+    }
+}
+
+foreach ($key in $this.TextAttribute.Keys) {
+    $textAttributes[$key] = $this.TextAttribute[$key]
+}
+
+return [xml]@(
+"<text id='$($this.ID)-text' $(
+    foreach ($TextAttributeName in $TextAttributes.Keys) {
+        " $TextAttributeName='$($TextAttributes[$TextAttributeName])'"
     }
 )>"
-            "<textPath href='#$($this.id)-path'>$([Security.SecurityElement]::Escape($this.Text))</textPath>"
-            if ($this.TextAnimation) {$this.TextAnimation}
-        "</text>"
-    ) -as [xml]
+if ($this.TextAnimation) {$this.TextAnimation}
+if ($this.Steps) {
+    "<textPath href='#$($this.id)-path'>$([Security.SecurityElement]::Escape($this.Text))</textPath>"
+} else {
+    $([Security.SecurityElement]::Escape($this.Text))
 }
+"</text>"
+)
+
 

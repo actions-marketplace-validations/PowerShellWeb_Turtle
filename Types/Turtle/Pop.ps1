92dec8791629cecbeac3bd13eee6531dc71b76bf
StartAutomating <james.brundage@start-automating.com>
2025-09-29 14:20:41 -0700

    fix: `Turtle.Pop` output fix ( Fixes #264 )
    Also adding docs to push and pop

--- Types/Turtle/Pop.ps1
+++ Types/Turtle/Pop.ps1
@@ -1,12 +1,19 @@
-if ($this.'.Stack' -isnot [Collections.Stack]) {
-    return
-}
+<#
+.SYNOPSIS
+    Pops the Turtle Stack
+.DESCRIPTION
+    Pops the Turtle back to the last location and heading in the stack.    
 
-if ($this.'.Stack'.Count -eq 0) {
-    return
-}
+    By pushing and popping, we can draw multiple branches.
+.EXAMPLE
+    # Draws a T shape by pushing and popping
+    turtle rotate -90 forward 42 push rotate 90 forward 21 pop rotate -90 forward 21 show
+#>
+if ($this.'.Stack' -isnot [Collections.Stack]) { return }
+
+if ($this.'.Stack'.Count -eq 0) { return }
 
 $popped = $this.'.Stack'.Pop()
-$this.PenUp().Goto($popped.Position.X, $popped.Position.Y).PenDown()
+$null = $this.PenUp().Goto($popped.Position.X, $popped.Position.Y).PenDown()
 $this.Heading = $popped.Heading
 return $this
--- Types/Turtle/Push.ps1
+++ Types/Turtle/Push.ps1
@@ -1,7 +1,16 @@
+<#
+.SYNOPSIS
+    Pushes the Turtle Stack
+.DESCRIPTION
+    Pushes the current state of this Turtle onto a stack.
+
+    If this stack is popped, the Turtle will teleport back to the location where it was pushed.
+
+    By pushing and popping, we can draw multiple branches.
+#>
 if (-not $this.'.Stack') {
     $this | Add-Member NoteProperty '.Stack' ([Collections.Stack]::new()) -Force
 }
-
 $this.'.Stack'.Push(@{
     Position = [Ordered]@{X=$this.Position.X;Y=$this.Position.Y}
     Heading = $this.Heading

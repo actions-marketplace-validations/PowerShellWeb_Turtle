b17828fb550a3037c1506641c8b0cb2f3c05bf59
StartAutomating <james.brundage@start-automating.com>
2025-10-08 12:22:51 -0700

    feat: `Turtle.get_ScriptBlock` ( Fixes #253 )
    Removing unescaped ScriptBlock and DataBlock from output

--- Types/Turtle/get_ScriptBlock.ps1
+++ Types/Turtle/get_ScriptBlock.ps1
@@ -6,9 +6,14 @@
 
     All steps will become a fluent pipeline.
 .EXAMPLE
-    turtle SierpinskiTriangle 42 4 | 
-        Select-Object -ExpandProperty ScriptBlock
+    turtle SierpinskiTriangle 42 4 scriptBlock
 #>
-[ScriptBlock]::Create(
-    $this.Commands.Extent -join (' |' + [Environment]::NewLine + '    ')
-)
+[OutputType([ScriptBlock])]
+param()
+# Join all of our previous command extents into a fluent pipeline
+$stringifiedScript = $this.Commands.Extent -join 
+    (' |' + [Environment]::NewLine + '    ') -replace # and then replace any unescaped use of 'ScriptBlock' or 'DataBlock'
+    "(?<!\[)(?>$(
+        'ScriptBlock', 'DataBlock' -join '|'
+    ))(?!\])\s{0,}"
+[ScriptBlock]::Create($stringifiedScript)

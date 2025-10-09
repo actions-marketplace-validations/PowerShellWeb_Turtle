d2678b0f8020c1242ebd89cf6b753323e3e63ccd
StartAutomating <james.brundage@start-automating.com>
2025-10-09 15:41:48 -0700

    fix: `Turtle.get/set_ViewBox` negative bounds ( Fixes #19 )

--- Types/Turtle/get_ViewBox.ps1
+++ Types/Turtle/get_ViewBox.ps1
@@ -1,8 +1,23 @@
+<#
+.SYNOPSIS
+    Gets the Turtle's viewbox
+.DESCRIPTION
+    Gets the Turtle's current viewBox.
+
+    If this has not been set, it will be automatically calculated by the minimum and maximum
+.NOTES
+    turtle square 42 viewbox
+#>
+
+param()
+
+# If we have set a viewbox, return it.
 if ($this.'.ViewBox') { return $this.'.ViewBox' }
 
-$viewX = $this.Maximum.X + ($this.Minimum.X * -1)
-$viewY = $this.Maximum.Y + ($this.Minimum.Y * -1)
+# Otherwise, subtract max from minimum to get a bounding box
+$viewBox = $this.Maximum - $this.Minimum
 
-return 0, 0, $viewX, $viewY
+# and return the viewbox
+return $this.Minimum.X, $this.Minimum.Y, $viewBox.width, $viewBox.height
 
 
